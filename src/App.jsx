6b8b19b1c7451c7caa4580619dc1491af8529581
jaycelab <jayxalvarado@gmail.com>
2024-03-18 16:21:30 -0700

    init new project

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import Footer from "../src/components/Footer";
 
 //Enclosing the components inside a fragment using <> </>. Div tags can also be used
 function App() {
+  const name = "Jason ";
+  const element = <h1>Hello world, {name}!</h1>;
   return (
     <>
+      <h1>{element}</h1>
       <Welcome />
       <Header />
       <Footer />
